ff0b28c994ba6fa6adc131c0a3453c077fa5a7e5
guilherme-menhard <guilhermemenhard.aluno@unipampa.edu.br>
2025-11-15 11:19:05 -0300

    Recuperacao-de-semana-passada-Gui
    Recuperando as classes que o Lucas apagou kkkkkk

---
+++ src/test/java/ListCursoTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import pages.ListCursoPage;
+import utils.Authentication;
+
+public class ListCursoTest {
+
+    WebDriver driver;
+    Authentication authentication;
+
+    @BeforeEach
+    void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        authentication = new Authentication(driver);
+        authentication.realizarLoginViaIndexedBD(); // login via IndexedDB (já funcional no seu projeto)
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) driver.quit();
+    }
+
+    // RF48 – Selecionar Vídeo (ver se o vídeo aparece ao entrar no curso)
+    @Test
+    public void CT48() {
+        ListCursoPage course = new ListCursoPage(driver);
+
+        course.abrirPaginaCursos()
+              .abrirCurso("Teste")   // abre o curso
+              .clicarVerVideoPorTitulo("Selenium com Java");
+        assertTrue(course.videoEstaNaTela(),
+                "O vídeo não apareceu na tela do curso.");
+    }
+
+    // RF49 – Assistir Vídeo (clicar para assistir e confirmar que carregou)
+    @Test
+    void CT49() throws InterruptedException {
+        ListCursoPage course = new ListCursoPage(driver);
+
+        course.abrirPaginaCursos()
+              .abrirCurso("Teste")
+              .clicarVerVideoPorTitulo("Selenium com Java");
+
+        assertTrue(course.videoCarregou(), "O vídeo não carregou.");
+        Thread.sleep(3000); // simula assistir
+    }
+}
